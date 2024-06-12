9bcdd1c93d881a591678f9a582425cfdd9e7c947
achchuthan-suntharalingam <achchu0103@gmail.com>
2024-06-12 13:16:28 +0530

    uuid length update

--- src/components/TicketBooking/getTickets.js
+++ src/components/TicketBooking/getTickets.js
@@ -16,7 +16,7 @@ import QRCode from 'qrcode';
 import ShortUniqueId from 'short-unique-id';
 
 export default function GetTickets() {
-  const uid = new ShortUniqueId({ dictionary: 'number' });
+  const uid = new ShortUniqueId({ dictionary: 'number',length:10 });
   const componentRef = useRef();
   const userDetails = useContext(UserDataContext);
   const userTheatreId = userDetails[0].theatreId;
