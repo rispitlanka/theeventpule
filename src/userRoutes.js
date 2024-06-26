eabf971c86e9bdb2b0fe6478bbc0cad68a23dce2
achchuthan-suntharalingam <achchu0103@gmail.com>
2024-06-26 10:55:45 +0530

    login delay event -1

--- src/userRoutes.js
+++ src/userRoutes.js
@@ -4,117 +4,119 @@ import routes from 'routes';
 
 export default function useUserRoutes() {
   const [userRoutes, setUserRoutes] = useState([]);
-  let userEmailWithQuotes = localStorage.getItem('userEmail');
+  const [isLoading, setIsLoading] = useState(true);
+  const userEmailWithQuotes = localStorage.getItem('userEmail');
 
   useEffect(() => {
     const fetchUserRoutes = async () => {
       try {
+        setIsLoading(true);
         let filteredRoutes = [];
         const userEmail = userEmailWithQuotes && userEmailWithQuotes.substring(1, userEmailWithQuotes.length - 1);
-        if (userEmail) {
-          const { data, error } = await supabase.from('theatreOwners').select('userRole').eq('email', userEmail);
-          const { userRole } = data[0];
-          if (error) {
-            console.log(error)
-          };
-          filteredRoutes = routes && routes.filter(route => {
-            if (userRole === 'superAdmin') {
-              return (
-                route.route !== '/authentication/sign-in' &&
-                route.route !== '/authentication/sign-up' &&
-                route.route !== '/theatre' &&
-                route.route !== '/bookings' &&
-                route.route !== '/view-tickets' &&
-                route.route !== '/events' &&
-                route.route !== '/theatres/single-theatre/add-screen/:id' &&
-                route.route !== '/theatres/single-theatre/edit-screen/:screenId' &&
-                route.route !== '/theatres/single-theatre/single-screen/add-showTime/:screenId' &&
-                route.route !== '/theatres/single-theatre/single-screen/edit-showTime/:showTimeId' &&
-                route.route !== '/theatres/single-theatre/single-screen/add-zone/:screenId' &&
-                route.route !== '/theatres/single-theatre/single-screen/single-zone/edit-zone/:id' &&
-                route.route !== '/bookings/book-seats/:showId/:screenId' &&
-                route.route !== '/bookings/book-seats/get-tickets' &&
-                route.route !== '/events/add-event' &&
-                route.route !== '/events/edit-event/:id' &&
-                route.route !== '/events/add-mainEvent' &&
-                route.route !== '/events/edit-mainEvent/:id' &&
-                route.route !== '/events' &&
-                route.route !== '/shows'
-              );
-            };
-            if (userRole === 'admin') {
-              return (
-                route.route !== '/authentication/sign-in' &&
-                route.route !== '/authentication/sign-up' &&
-                route.route !== '/users' &&
-                route.route !== '/customers' &&
-                route.route !== '/theatres' &&
-                route.route !== '/theatreMasterData' &&
-                route.route !== '/facilities' &&
-                route.route !== '/movies' &&
-                route.route !== '/movieMasterData' &&
-                route.route !== '/languages' &&
-                route.route !== '/genre' &&
-                route.route !== '/censor-types' &&
-                route.route !== '/soundsystem' &&
-                route.route !== '/projection-type' &&
-                route.route !== '/cast' &&
-                route.route !== '/crew' &&
-                route.route !== '/bookingsAdmin' &&
-                route.route !== '/facilities/add-facilities' &&
-                route.route !== '/movies/add-movie' &&
-                route.route !== '/languages/add-languages' &&
-                route.route !== '/cast/add-cast' &&
-                route.route !== '/crew/add-crew"' &&
-                route.route !== '/genre/add-genre' &&
-                route.route !== '/censor-types/add-censor-types' &&
-                route.route !== '/languages/edit-languages/:id' &&
-                route.route !== '/cast/edit-cast/:id' &&
-                route.route !== '/movies/edit-movie/:id' &&
-                route.route !== '/crew/edit-crew/:id' &&
-                route.route !== '/soundsystem/add-soundsystem' &&
-                route.route !== '/projection-type/add-projection-type"' &&
-                route.route !== '/tables/add-owner' &&
-                route.route !== '/tables/manage-owner' &&
-                route.route !== '/theatres/add-theatre' &&
-                route.route !== '/theatres/edit-theatre/:id' &&
-                route.route !== '/facilities/edit-facilities/:id' &&
-                route.route !== '/genre/edit-genre/:id' &&
-                route.route !== '/censor-types/edit-censor-types/:id"' &&
-                route.route !== '/soundsystem/edit-soundsystem/:id' &&
-                route.route !== '/projection-type/edit-projection-type/:id' &&
-                route.route !== '/bookingsAdmin' &&
-                route.route !== '/bookingsAdmin' &&
-                route.route !== '/bookingsAdmin' &&
-                route.route !== '/bookingsAdmin' &&
-                route.route !== '/bookingsAdmin' &&
-                route.route !== '/bookingsAdmin' &&
-                route.route !== '/bookingsAdmin' &&
-                route.route !== '/bookingsAdmin' &&
-                route.route !== '/bookingsAdmin' &&
-                route.route !== '/bookingsAdmin' &&
 
-                route.route !== '/facilities'
-              );
+        if (userEmail) {
+          const { data, error } = await supabase
+            .from('theatreOwners')
+            .select('userRole')
+            .eq('email', userEmail);
+
+          if (error) {
+            console.error(error);
+            setIsLoading(false);
+            return;
+          }
+
+          const userRole = data[0]?.userRole || 'user';
+
+          filteredRoutes = routes.filter(route => {
+            if (userRole === 'superAdmin') {
+              return ![
+                '/authentication/sign-in',
+                '/authentication/sign-up',
+                '/theatre',
+                '/bookings',
+                '/view-tickets',
+                '/events',
+                '/theatres/single-theatre/add-screen/:id',
+                '/theatres/single-theatre/edit-screen/:screenId',
+                '/theatres/single-theatre/single-screen/add-showTime/:screenId',
+                '/theatres/single-theatre/single-screen/edit-showTime/:showTimeId',
+                '/theatres/single-theatre/single-screen/add-zone/:screenId',
+                '/theatres/single-theatre/single-screen/single-zone/edit-zone/:id',
+                '/bookings/book-seats/:showId/:screenId',
+                '/bookings/book-seats/get-tickets',
+                '/events/add-event',
+                '/events/edit-event/:id',
+                '/events/add-mainEvent',
+                '/events/edit-mainEvent/:id',
+                '/events',
+                '/shows',
+              ].includes(route.route);
+
+            } else if (userRole === 'admin') {
+              return ![
+                '/authentication/sign-in',
+                '/authentication/sign-up',
+                '/users',
+                '/customers',
+                '/theatres',
+                '/theatreMasterData',
+                '/facilities',
+                '/movies',
+                '/movieMasterData',
+                '/languages',
+                '/genre',
+                '/censor-types',
+                '/soundsystem',
+                '/projection-type',
+                '/cast',
+                '/crew',
+                '/bookingsAdmin',
+                '/facilities/add-facilities',
+                '/movies/add-movie',
+                '/languages/add-languages',
+                '/cast/add-cast',
+                '/crew/add-crew',
+                '/genre/add-genre',
+                '/censor-types/add-censor-types',
+                '/languages/edit-languages/:id',
+                '/cast/edit-cast/:id',
+                '/movies/edit-movie/:id',
+                '/crew/edit-crew/:id',
+                '/soundsystem/add-soundsystem',
+                '/projection-type/add-projection-type',
+                '/tables/add-owner',
+                '/tables/manage-owner',
+                '/theatres/add-theatre',
+                '/theatres/edit-theatre/:id',
+                '/facilities/edit-facilities/:id',
+                '/genre/edit-genre/:id',
+                '/censor-types/edit-censor-types/:id',
+                '/soundsystem/edit-soundsystem/:id',
+                '/projection-type/edit-projection-type/:id',
+                '/bookingsAdmin',
+                '/facilities',
+              ].includes(route.route);
             }
             return route.route === '/dashboard';
           });
-          setUserRoutes(filteredRoutes);
-        }
-        else if (!userEmail) {
-          filteredRoutes = routes && routes.filter(route => {
-            return route.route === '/authentication/sign-in' || route.route === "/register/:eventId";
+
+        } else {
+          filteredRoutes = routes.filter(route => {
+            return route.route === '/authentication/sign-in' || route.route === '/register/:eventId';
           });
-          setUserRoutes(filteredRoutes);
         }
+        setUserRoutes(filteredRoutes);
       } catch (error) {
         console.error('Error fetching user routes:', error.message);
+      } finally {
+        setIsLoading(false);
       }
-
     };
 
     fetchUserRoutes();
   }, [userEmailWithQuotes]);
 
-  return userRoutes;
+  return { userRoutes, isLoading };
 }
+
