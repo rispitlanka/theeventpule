22c4e63bde0b7b1d1ffb95114b06cc5a939e7c10
achchuthan-suntharalingam <achchu0103@gmail.com>
2024-07-30 11:54:50 +0530

    zone tickets count

--- src/components/EventBooking/ticketsCountModel.js
+++ src/components/EventBooking/ticketsCountModel.js
@@ -1,4 +1,4 @@
-import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import PropTypes from "prop-types";
 import { useNavigate } from 'react-router-dom';
@@ -11,6 +11,7 @@ export default function TicketsCountModel({ open, handleClose, eventId, venueId,
     const navigate = useNavigate();
     const [venueData, setVenueData] = useState([]);
     const [selectedZoneId, setSelectedZoneId] = useState(null);
+    const [bookedTicketsCount, setBookedTicketsCount] = useState(0);
 
     const fetchVenue = async () => {
         try {
@@ -37,6 +38,35 @@ export default function TicketsCountModel({ open, handleClose, eventId, venueId,
     const fullTicketPrice = selectedZoneId && (venueData[0]?.zones_events?.filter(zone => zone.id === selectedZoneId).map(zone => zone.price)[0]);
     const halfTicketPrice = selectedZoneId && (venueData[0]?.zones_events?.filter(zone => zone.id === selectedZoneId).map(zone => zone.halfPrice)[0]);
     const zoneName = selectedZoneId && (venueData[0]?.zones_events?.filter(zone => zone.id === selectedZoneId).map(zone => zone.name)[0]);
+    const totalZoneTicketsCount = selectedZoneId && (venueData[0]?.zones_events?.filter(zone => zone.id === selectedZoneId).map(zone => zone.ticketsCount)[0]) || 0;
+
+    useEffect(() => {
+        const fetchBookedTicketsCount = async (selectedZoneId, eventId) => {
+            try {
+                const { data, error } = await supabase
+                    .rpc('get_booked_tickets_count', { zone_id: selectedZoneId, event_id: eventId });
+                if (data) {
+                    const bookedSeatsCount = data[0].booked_tickets_count;
+                    // setBookedTicketsCount(prevCounts => ({
+                    //     ...prevCounts,
+                    //     [eventId]: {
+                    //         ...(prevCounts[selectedZoneId] || {}),
+                    //         [eventId]: bookedSeatsCount
+                    //     }
+                    // }));
+                    setBookedTicketsCount(bookedSeatsCount);
+                }
+                if (error) {
+                    console.log(error);
+                    return 0;
+                }
+            } catch (error) {
+                console.log('Error in fetching booked tickets', error);
+                return 0;
+            }
+        };
+        fetchBookedTicketsCount(Number(selectedZoneId), eventId);
+    }, [selectedZoneId, eventId])
 
     const ticketsCount = useFormik({
         initialValues: {
@@ -44,8 +74,23 @@ export default function TicketsCountModel({ open, handleClose, eventId, venueId,
             halfTicketsCount: '',
         },
         validationSchema: Yup.object({
-            fullTicketsCount: Yup.number().required('Required').min(1, 'Must be at least 1'),
-            halfTicketsCount: Yup.number().min(0, 'Cannot be less than 0'),
+            fullTicketsCount: Yup
+                .number()
+                .required('Required')
+                .min(0)
+                .test('max-tickets', 'Exceeds available tickets', function (value) {
+                    const { halfTicketsCount } = this.parent;
+                    const maxAvailableTickets = totalZoneTicketsCount - bookedTicketsCount;
+                    return value + (halfTicketsCount || 0) <= maxAvailableTickets;
+                }),
+            halfTicketsCount: Yup
+                .number()
+                .min(0)
+                .test('max-tickets', 'Exceeds available tickets', function (value) {
+                    const { fullTicketsCount } = this.parent;
+                    const maxAvailableTickets = totalZoneTicketsCount - bookedTicketsCount;
+                    return value + (fullTicketsCount || 0) <= maxAvailableTickets;
+                }),
         }),
         onSubmit: (values) => {
             navigate('/eventBookings/book-seats/get-tickets', {
@@ -67,6 +112,10 @@ export default function TicketsCountModel({ open, handleClose, eventId, venueId,
         },
     });
 
+    useEffect(() => {
+        ticketsCount.resetForm();
+    }, [selectedZoneId]);
+
     return (
         <Dialog open={open} onClose={handleClose}>
             <form onSubmit={ticketsCount.handleSubmit}>
@@ -75,8 +124,8 @@ export default function TicketsCountModel({ open, handleClose, eventId, venueId,
                     <DialogContentText>
                         Choose the perfect tickets to suit your needs!
                     </DialogContentText>
-                    <MDBox>
-                        <Box m={1}>
+                    <MDBox m={1}>
+                        <Box>
                             <FormControl fullWidth>
                                 <InputLabel>Select Zone</InputLabel>
                                 <Select
@@ -94,6 +143,7 @@ export default function TicketsCountModel({ open, handleClose, eventId, venueId,
                                 </Select>
                             </FormControl>
                         </Box>
+                        <Box><Typography>{bookedTicketsCount}/{totalZoneTicketsCount}</Typography></Box>
                         <TextField
                             // autoFocus
                             required
